996857fdf188aab20ccbe7ef8751cbe077a70fa4
phhernandes <pedrobeco97@gmail.com>
2023-04-14 14:40:08 -0300

    commit

---
+++ js/rolagem.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll', function() {
+    const timeInfos = document.querySelectorAll('#section-chave li .time-info');
+    for (const timeInfo of timeInfos) {
+      if (isElementInViewport(timeInfo)) {
+        timeInfo.style.opacity = 1;
+        timeInfo.style.bottom = '5%';
+      } else {
+        timeInfo.style.opacity = 0;
+        timeInfo.style.bottom = '-20px';
+      }
+    }
+  });
+  
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
